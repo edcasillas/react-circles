ca4d040a6f8ef71f556dfd9148c89635d1e8beaf
Ed <eduardo_casillas@hotmail.com>
2022-11-27 11:09:25 -0800

    progress handling inputs

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 import { useRef } from "react";
 import { drawCircle, drawLine } from "./figure-drawers";
+import { getEventLocation } from "./input-utils";
+
+interface Location {
+    x: number;
+    y: number;
+}
 
 const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -8,18 +14,20 @@ const Canvas = () => {
     useEffect(()=> {
         //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
         //youtube.com/watch?v=K8HICLm-Jj0
+        // https://codepen.io/chengarda/pen/wRxoyB
         const canvas = canvasRef.current;
         if(!canvas) return;
 
-        canvas.width = 640;
-        canvas.height = 480;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        canvas.addEventListener('mousedown', onPointerDown);
+        canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
 
         const context = canvas.getContext('2d');
         if(!context) return;
 
         context.fillStyle = 'blue';
-        /*context.fillRect(0,0,100,100);
-        context.strokeRect(100,100,50,50);*/
 
         drawLine(context, 100,35,200,55);
         drawCircle(context, 100, 35, 10);
@@ -27,7 +35,25 @@ const Canvas = () => {
         
     }, []);
 
-    return <canvas ref={canvasRef} />;
+    function onPointerDown(e : MouseEvent) {
+        /*isDragging = true
+        dragStart.x = getEventLocation(e).x/cameraZoom - cameraOffset.x
+        dragStart.y = getEventLocation(e).y/cameraZoom - cameraOffset.y*/
+        const pointerLocation = getEventLocation(e);
+        console.log(pointerLocation);
+    }
+
+    function handleTouch(e : TouchEvent, singleTouchHandler : (this: HTMLCanvasElement, ev: MouseEvent) => any) {
+        /*if ( e.touches.length == 1 ) {
+            singleTouchHandler(e)
+        }
+        else if (e.type == "touchmove" && e.touches.length == 2) {
+            isDragging = false
+            handlePinch(e)
+        }*/
+    }
+
+    return <canvas ref={canvasRef} className={'main_canvas'} />;
 };
 
 export default Canvas;
---
+++ src/input-utils.tsx
@@ -0,0 +1,15 @@
+/**
+ * Gets the relevant location from a mouse or single touch event
+ */
+export function getEventLocation(e : MouseEvent | TouchEvent) : {x: number, y: number} {
+    if(e instanceof TouchEvent) {
+        if (e.touches && e.touches.length == 1) {
+            return { x:e.touches[0].clientX, y: e.touches[0].clientY }
+        }
+    } else {
+        if (e.clientX && e.clientY) {
+            return { x: e.clientX, y: e.clientY }
+        }
+    }
+    return {x: 0, y: 0};
+}
